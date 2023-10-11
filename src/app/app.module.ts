f4166534970c7bd3eeb4be2221ea161223a530c0
aloha2 <5oE75DaIZhcls>
2023-10-11 09:35:44 +0530

    Basic todo-List for understandings of Angular
    This todo-;ist is just very basuce for learning some Angular uses.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './Components/todos/todos.component';
+import { TodoItemComponent } from './Components/todo-item/todo-item.component';
+import { AddtodoComponent } from './Components/addtodo/addtodo.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddtodoComponent,
+    AboutComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
